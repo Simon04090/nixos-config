27820ed70041188fd42606b24b070ea79d2cf53b
Simon Kammermeier <simon.kammermeier@tum.de>
2026-06-25 13:57:44 +0200

    Add home manager

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager }:
     let username = "simon";
     in {
 
@@ -20,6 +24,15 @@
         };
       };
 
+      homeConfigurations.simon = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          ./home.nix
+        ];
+        extraSpecialArgs = {
+          inherit username;
+        };
+      };
 
   };
 }
---
+++ home.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, username, ... }:
+{
+  home.packages = with pkgs; [
+      nix-output-monitor
+  ];
+
+  home.username = username;
+
+  home.homeDirectory = "/home/${username}";
+
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "Simon Kammermeier";
+          email = "simon.kammermeier@tum.de";
+        };
+      };
+    };
+
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+  };
+
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
